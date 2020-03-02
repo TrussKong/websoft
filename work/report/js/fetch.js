08a481ada0e559c3334f178a6c60da33b069b998
TrussKong <Daniel.kongdet@Gmail.com>
2020-03-02 16:05:15 +0100

    dynamic table with fetch from local JSON

--- work/report/js/fetch.js
+++ work/report/js/fetch.js
@@ -3,7 +3,6 @@
 
     let url;
 
-    //url = "https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1081";
     url = "data/1081.json";
     fetch(url)
         .then((response) => {
@@ -15,14 +14,7 @@
 
         console.log('Sandbox MEGA is ready');
 
-//     url = "https://rem.dbwebb.se/api/users";
-//     fetch(url)
-//         .then((response) => {
-//             return response.json();
-//         })
-//         .then((myJson) => {
-//             console.log(myJson);
-//         });
-
-//     console.log('Sandbox is ready!');
  })();
+
+
+
